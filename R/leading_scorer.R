d1a691bdb6bfa5f2826e60d613386778beb2eeee
Asahi Nino <nino.asahi@gmail.com>
2021-03-08 11:40:41 -0800

    reup all

---
+++ R/leading_scorer.R
@@ -0,0 +1,12 @@
+#' An rnbapackage function
+#'
+#' This function allows you to get the leading scorer by total points from a given season.
+#' @param year The season from which you want the leading scorer.
+#' @keywords leading scorer
+#' @export
+#' @examples
+#' leading_scorer()
+leading_scorer <- function(year) {
+  nba_year <- nba_stats %>% dplyr::filter(Year == year)
+  return(nba_year %>% dplyr::filter(PTS == max(nba_year$PTS)))
+}
